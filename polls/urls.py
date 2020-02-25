6a5f6515822d47da0461980506965e57a0be9bfd
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-02-25 00:33:18 -0500

    Added index page + beginning of view for ice cream question

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:questionID>/', views.detail, name='detail'),
+    path('<int:questionID>/results/', views.results, name='results'),
+    path('<int:questionID>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pdate')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }    
+    return render(request, 'polls/index.html', context)
+
 # Create your views here.
+
+def detail(request, questionID):
+    return HttpResponse("I have one simple question for you: %s" % questionID)
+
+def results(request, questionID):
+    response = "For those who answered %s"
+    return HttpResponse(response % questionID)
+
+def vote(request, questionID):
+    return HttpResponse("So tell me: %s" % questionID)
